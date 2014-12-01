3cc170494c932381973a70d649e5bce3a3d6a536
mbrendsel <monica_brendsel@asl.org>
2014-12-01 22:24:06 +0000

    Update Lab06.java

--- Lab06.java
+++ Lab06.java
@@ -28,60 +28,60 @@
       }
        public static void task_01()	
       { //go to the end of the row of beepers
-         Robot temp = new Robot(1, 1, Display.EAST, 0);
-      while(temp.nextToABeeper()) {
-      temp.move(); }
+         Robot monica = new Robot(1, 1, Display.EAST, 0);
+      while(monica.nextToABeeper()) {
+      monica.move(); }
       }
        public static void task_02()	
       { //go to the beeper
-         Robot temp = new Robot(1, 2, Display.EAST, 0);
-      	while(!temp.nextToABeeper()) {
-         temp.move(); }
+         Robot bob = new Robot(1, 2, Display.EAST, 0);
+      	while(!bob.nextToABeeper()) {
+         bob.move(); }
       }
        public static void task_03()	
       { //go to the wall
-         Robot temp = new Robot(1, 3, Display.EAST, 0);
-         while(temp.frontIsClear()) {
-         temp.move(); }
+         Robot max = new Robot(1, 3, Display.EAST, 0);
+         while(max.frontIsClear()) {
+         max.move(); }
       }
        public static void task_04()	
       { //go to the wall, pick up all the beepers (max one per pile)
-         Robot temp = new Robot(1, 4, Display.EAST, 0);
-         while(temp.frontIsClear()) {
-         while(temp.nextToABeeper()) {
-         temp.pickBeeper(); }
-         temp.move();
+         Robot rachel = new Robot(1, 4, Display.EAST, 0);
+         while(rachel.frontIsClear()) {
+         while(rachel.nextToABeeper()) {
+         rachel.pickBeeper(); }
+         rachel.move();
          }
-         if(!temp.frontIsClear() && temp.nextToABeeper()) {
-         temp.pickBeeper();
+         if(!rachel.frontIsClear() && rachel.nextToABeeper()) {
+         rachel.pickBeeper();
       }
       }
        public static void task_05()	
       { //go to the wall, pick up all the beepers
-         Robot temp = new Robot(1, 5, Display.EAST, 0);
-         while(temp.frontIsClear()) {
-         while(temp.nextToABeeper()) {
-         temp.pickBeeper(); }
-         temp.move();
+         Robot kelly = new Robot(1, 5, Display.EAST, 0);
+         while(kelly.frontIsClear()) {
+         while(kelly.nextToABeeper()) {
+         kelly.pickBeeper(); }
+         kelly.move();
          }
-         while(!temp.frontIsClear() && temp.nextToABeeper()) 
+         while(!kelly.frontIsClear() && kelly.nextToABeeper()) 
          {
-         temp.pickBeeper();
+         kelly.pickBeeper();
          }
 
       }
        public static void task_06()
       { //go to the end of the row of beepers, there is one gap
-         Robot temp = new Robot(1, 6, Display.EAST, 0);
-         while(temp.nextToABeeper()) 
+         Robot jim = new Robot(1, 6, Display.EAST, 0);
+         while(jim.nextToABeeper()) 
          {
-         temp.move(); 
+         jim.move(); 
          }
-         if(temp.frontIsClear()) 
+         if(jim.frontIsClear()) 
          {
-         temp.move();
-         while(temp.nextToABeeper()) {
-         temp.move();
+         jim.move();
+         while(jim.nextToABeeper()) {
+         jim.move();
          }
          }
       }
